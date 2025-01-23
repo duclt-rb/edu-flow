d834f7ca0b407df50f57cf9f6da805f353659282
Thành Đức <87427318+duclt-rb@users.noreply.github.com>
2025-01-23 13:08:17 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(
